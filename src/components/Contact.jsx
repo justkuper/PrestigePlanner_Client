6ec7a390d61c723f99dea62cb8f69fcabb98e81e
WickedDan <daniel@wickedflow.com>
2024-09-03 10:00:41 -0700

    Form-Functtionalities
    Added For Functionalities
    
    *CSS needed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,48 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 
-function Contact() {
+export default function Contact() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "a60564ee-a072-49e5-8afb-e25dd322a44f");
+
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+
+        const res = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: json
+        }).then((res) => res.json());
+
+        if (res.success) {
+            console.log("Success", res);
+        }
+        window.alert('Success, Form Submitted!', formData);
+        setFormData({ name: '', email: '', message: '' });
+    };
+
     return (
-        <div className="container d-flex flex-column justify-content-center min-vh-100">
-            <div className="row">
-                <div className="col-md-6 offset-md-3">
-                <h1 className="title">Contact</h1>
-                    <form
-                        action="https://getform.io/f/(customSlugHere)"
-                        method="POST"
-                        className="d-flex flex-column"
-                    >
-                        <input
-                            type="text"
-                            name="name"
-                            placeholder="Enter Name"
-                            className="inputField glow"
-                        />
-                        <input
-                            type="text"
-                            name="email"
-                            placeholder="Enter Email"
-                            className="inputField glow"
-                        />
-                        <textarea
-                            name="message"
-                            placeholder="Enter Message (400) Character Max"
-                            rows="10"
-                            className="textareaField glow"
-                        />
-                        <button 
-                            type="button"
-                            className="submitButton"
-                        >
-                            Let's Unite
-
-                        </button>
-                    </form>
+        <div>
+            <form className="container d-flex flex-column justify-content-center min-vh-100" onSubmit={handleSubmit}>
+                <div>
+                    <h1 className="title">Contact</h1>
+                    <label className="form-label bg-light" htmlFor="name">Name</label>
+                    <input className="form-control"
+                        type="text"
+                        id="name"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        required
+                    />
                 </div>
-            </div>
-        </div>
-    )
-}
+                <div>
+                    <label className="form-label bg-light" htmlFor="email">Email</label>
+                    <input className="form-control"
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className>
+                    <label className="form-label bg-light" htmlFor="message">Message</label>
+                    <textarea className="form-control"
+                        id="message"
+                        name="message"
+                        value={formData.message}
+                        onChange={handleChange}
+                        rows="4"
+                        required
+                    ></textarea>
+                </div>
+                <button className="mt-2 mb-2 submitButton" type="submit">Let's Unite</button>
+            </form>
 
-export default Contact;
+            <footer>
+                <div className="d-flex justify-content-center">
+                    <a className="p-5" href="mailto:daniel@wickedflow.com">Email</a>
+                    <a className="p-5" href="https://github.com/WickedDan">Github</a>
+                    <a className="p-5 " href="https://www.instagram.com/daniel_h_contreras/">Instagram</a>
+                </div>
+            </footer>
+        </div>
+    );
+}
