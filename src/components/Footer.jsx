395b9acb2f78ad815cacc5e73599c0adbab7a1f0
justkuper <kbank1988@gmail.com>
2024-09-10 10:56:24 -0700

    fixed all events page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -60,7 +60,7 @@ export default function Footer() {
       <div className="container">
         <section className="social-icons-wrapper d-flex flex-column flex-md-row justify-content-center my-3">
           {/* DANIEL'S SECTION */}
-          <div className="d-flex flex-column align-items-center mx-md-2 transparent-bg">
+          <div className="d-flex flex-column align-items-center mx-md-2 transparent-bg ">
             <a href="https://github.com/WickedDan" className="icon Daniel" target="_blank" rel="noreferrer">
               <FontAwesomeIcon icon={faGithub} flip size="3x" style={{ color: 'green' }} />
             </a>
@@ -98,18 +98,16 @@ export default function Footer() {
           {/* JOHANNES SECTION */}
           <div className="d-flex flex-column align-items-center mx-md-2 transparent-bg">
             <a href="https://github.com/johannesmstoppler" className="icon johannesmstoppler" target="_blank" rel="noreferrer">
-              <FontAwesomeIcon icon={faGithub} bounce size="3x" style={{ color: 'orange' }} />
+              <FontAwesomeIcon icon={faGithub} flip size="3x" style={{ color: 'orange' }} />
             </a>
             <a href="https://github.com/johannesmstoppler" target="_blank" rel="noreferrer">
               <span className="d-inline-block mx-1 name">@johannesmstoppler</span>
             </a>
           </div>
-        </section>
-
-        {/* Reviews Button */}
+          {/* Reviews Button */}
         <button
-          className="btn btn-primary"
-          style={{ position: 'absolute', bottom: '-715px', left: '20px' }}
+          className="btn btn-primary review-btn"
+          style={{ display: 'flex', position: 'absolute', bottom: '-625px', left: '20px' }}
           onClick={() => setShowModal(true)}
         >
           Leave a Review
@@ -177,17 +175,20 @@ export default function Footer() {
             </div>
           </div>
         )}
+        </section>
+
+        
         {/* Share Icons */}
 
         <section className="footer" id="footer">
           <section className="form-footer">
-            <h5 className="text-center mb-2">Designed by Fab Five  </h5>
-            <div className="text-center mb-0">
+            <h5 className="text-center mb-2">Designed by Fab Five  &copy; {new Date().getFullYear()} All rights reserved.</h5>
+            {/* <div className="text-center mb-0">
               <h6>&copy; {new Date().getFullYear()} All rights reserved.</h6>
-            </div>
+            </div> */}
 
-          </section>
-          <section className="share-icon">
+          
+          <section className="share-icon" >
             <a href="https://wa.me/?text=Check%20out%20Prestigle%20Planners!%20https://prestigleplanners.com" target="_blank" rel="noreferrer">
               <FontAwesomeIcon icon={faWhatsapp} size="3x" style={{ color: 'green', marginRight: '20px' }} />
             </a>
@@ -197,6 +198,7 @@ export default function Footer() {
             <a href="https://www.instagram.com/?url=https://prestigleplanners.com" target="_blank" rel="noreferrer">
               <FontAwesomeIcon icon={faInstagram} size="3x" style={{ color: 'purple' }} />
             </a>
+          </section>
           </section>
         </section>
 
